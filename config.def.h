fc151854a7e1e6bfbd098ff2da40ea63cbd49455
Syed Shah <syed@hp-1>
2021-01-15 10:49:23 +0000

    radius trivial

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int cornerrad = 7;
+static const unsigned int cornerrad = 10;
 static const unsigned int gappih    = 5;
 static const unsigned int gappiv    = 5;
 static const unsigned int gappoh    = 5;
